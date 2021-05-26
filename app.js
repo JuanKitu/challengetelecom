d2a96cb04d616c21bfc589ea3e84662891dbfb80
Juan Manuel <juanmix.01@gmail.com>
2021-05-26 18:48:00 -0300

    feat: Se agrego y configuro el swagger

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const path = require('path');
 const AutoLoad = require('fastify-autoload');
+const swagger = require('fastify-swagger');
 
 module.exports = async (fastify, opts) => {
   // Place here your custom code!
 
+  fastify.register(swagger, {
+    routePrefix: '/v1',
+    swagger: {
+      info: {
+        title: 'Challenge de Back End Telecom',
+        description: 'Una pequeña api rest para el pronostico del tiempo',
+        version: '0.1.0',
+      },
+      host: '192.168.0.116',
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json'],
+    },
+    uiConfig: {
+      docExpansion: 'list',
+      deepLinking: false,
+      validatorUrl: null,
+    },
+    exposeRoute: true,
+  });
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
