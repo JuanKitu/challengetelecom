74e2d632022cdd1cecd5177184656446be3e6bc3
Juan Manuel <juanmix.01@gmail.com>
2021-05-26 20:36:49 -0300

    feat: Se creo la funcion getTiempoActual

--- function/funcionesBasicas.js
+++ function/funcionesBasicas.js
@@ -8,4 +8,22 @@ function dataIp(ip) {
   });
 }
 
-module.exports = { dataIp };
+function getTiempoActual(latitud, longitud) {
+  // eslint-disable-next-line no-async-promise-executor
+  return new Promise(async (resolve) => {
+    const tiempoConfig = {
+      latitud,
+      longitud,
+      apiID: 'a63072e3e1e01801c117e4a397ba5302',
+      exclude: 'hourly,minutely,daily',
+      units: 'metric',
+      lang: 'es',
+    };
+    const tiempo = `https://api.openweathermap.org/data/2.5/onecall?lat=${tiempoConfig.latitud}&lon=${tiempoConfig.longitud}&exclude=${tiempoConfig.exclude}&units=${tiempoConfig.units}&lang=es&appid=${tiempoConfig.apiID}`;
+    await fetch(tiempo).then((res) => res.json()).then((geo) => {
+      resolve(geo);
+    });
+  });
+}
+
+module.exports = { dataIp, getTiempoActual };
