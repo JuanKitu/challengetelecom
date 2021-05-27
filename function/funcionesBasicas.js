f0a447c55c4f83a6717f3b9093ce8d3f7ad98eaa
Juan Manuel <juanmix.01@gmail.com>
2021-05-26 21:12:02 -0300

    feat: Se creo la funcion getTiempoDias

--- function/funcionesBasicas.js
+++ function/funcionesBasicas.js
@@ -26,4 +26,22 @@ function getTiempoActual(latitud, longitud) {
   });
 }
 
-module.exports = { dataIp, getTiempoActual };
+function getTiempoDias(latitud, logitud) {
+  // eslint-disable-next-line no-async-promise-executor
+  return new Promise(async (resolve) => {
+    const tiempoConfig = {
+      latitud,
+      logitud,
+      apiID: 'a63072e3e1e01801c117e4a397ba5302',
+      exclude: 'hourly,minutely,current',
+      units: 'metric',
+      lang: 'es',
+    };
+    const tiempo = `https://api.openweathermap.org/data/2.5/onecall?lat=${tiempoConfig.latitud}&lon=${tiempoConfig.longitud}&exclude=${tiempoConfig.exclude}&units=${tiempoConfig.units}&lang=es&appid=${tiempoConfig.apiID}`;
+    await fetch(tiempo).then((res) => res.json()).then((geo) => {
+      resolve(geo);
+    });
+  });
+}
+
+module.exports = { dataIp, getTiempoActual, getTiempoDias };
