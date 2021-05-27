20df8dd38ce8b3160b65e28960d6ab2475624839
Juan Manuel <juanmix.01@gmail.com>
2021-05-26 23:17:48 -0300

    feat: Se creo el controlador de getCurrentCity

--- controllers/current.controller.js
+++ controllers/current.controller.js
@@ -1,11 +1,16 @@
-const { dataIp, getTiempoActual } = require('../function/funcionesBasicas');
+const {
+  dataIp,
+  getTiempoActual,
+  getCity,
+  dataLatLon,
+} = require('../function/funcionesBasicas');
 
 const controller = {};
 controller.getCurrent = async (request, reply) => {
   const { ip } = request;
   await dataIp(ip).then(async (data) => {
     if (data.status !== 'fail') {
-      await getTiempoActual(data.lat, data.lon).then((geo) => {
+      await getTiempoActual(data.latitud, data.longitud).then((geo) => {
         reply.status(200).send({
           err: false,
           city: data,
@@ -17,6 +22,23 @@ controller.getCurrent = async (request, reply) => {
       err: true,
       data,
     });
+  });
+};
+controller.getCurrentCity = async (request, reply) => {
+  const { city } = request.params;
+  const ciudad = getCity(city);
+  if (ciudad) {
+    await getTiempoActual(ciudad.latitud, ciudad.longitud).then((async (geo) => {
+      await dataLatLon(ciudad.latitud, ciudad.longitud).then((find) => reply.status(200).send({
+        err: false,
+        city: find,
+        current: geo,
+      }));
+    }));
+  }
+  return reply.status(404).send({
+    err: true,
+    message: 'no se encuentra en la BD',
   });
 };
 module.exports = controller;
